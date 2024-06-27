dc7d23434cd79981a9fd1d86f8c7f276dfc04aaf
mikepreston17 <michael.n.preston@gmail.com>
2024-06-27 05:37:06 -0500

    signup alert htmx working

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -3,8 +3,11 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace justdoit.Pages;
 
+[BindProperties]
 public class IndexModel : PageModel
 {
+    public string Email { get; set; } = string.Empty;
+    public string CC { get; set; } = string.Empty;
     private readonly ILogger<IndexModel> _logger;
 
     public IndexModel(ILogger<IndexModel> logger)
@@ -16,10 +19,11 @@ public class IndexModel : PageModel
     {
     }
 
-
-    public IActionResult OnPostSignup()
+    public async Task<IActionResult> OnPostSignup()
     {
         Console.WriteLine(nameof(OnPostSignup));
+        Console.WriteLine("email: " + Email);
+        Console.WriteLine("credit: " + CC);
         return Partial("_SignupThankYou");
     }
 }
