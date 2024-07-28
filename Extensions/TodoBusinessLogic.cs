ce1c9f9964695af711453a974077468bb9e16cce
mikepreston17 <michael.n.preston@gmail.com>
2024-07-27 20:05:28 -0500

    ++ new sorts

--- Extensions/TodoBusinessLogic.cs
+++ Extensions/TodoBusinessLogic.cs
@@ -1,4 +1,6 @@
+using CodeMechanic.Types;
 using justdoit.Models;
+using NSpecifications;
 
 namespace justdoit;
 
@@ -6,6 +8,23 @@ public static class TodoBusinessLogic
 {
     public static List<Todo> ApplyFilters(this List<Todo> items)
     {
-        return items;
+        bool sort_by_priority = true;
+        SortDirection priority_direction = SortDirection.Descending;
+
+        bool sort_by_due = true;
+        SortDirection due_direction = SortDirection.Descending;
+
+        return items
+            .If(sort_by_priority, todos =>
+                Equals(due_direction, SortDirection.Descending)
+                    ? todos.OrderByDescending(todo => todo.priority)
+                    : todos.OrderBy(todo => todo.priority)
+            )
+            .If(sort_by_due, todos =>
+                Equals(due_direction, SortDirection.Descending)
+                    ? todos.OrderByDescending(todo => todo.due)
+                    : todos.OrderBy(todo => todo.due)
+            )
+            .ToList();
     }
 }
--- Pages/Sandbox/Pocketbase.cshtml.cs
+++ Pages/Sandbox/Pocketbase.cshtml.cs
@@ -34,7 +34,8 @@ public class Pocketbase : PageModel
         if (debug) Console.WriteLine($"{nameof(Query)}: {Query}");
         if (debug) Console.WriteLine($"{nameof(query)}: {query}");
 
-        todos = (await db.GetAll()).ApplyFilters()
+        todos = (await db.GetAll())
+            .ApplyFilters()
             .ToList();
         // todos.Dump(nameof(OnGetSortedTreadmill));
         // todo: finish reseting the todos by the age of dueness.
